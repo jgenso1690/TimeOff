dd809eee1f9869330ebd7b54dc05cbbb04bd3063
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-06-06 17:43:26 -0400

    express, nodemon, server ready

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+const port = 4000;
+const bodyParser = require('body-parser');
+
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(express.static('public'))
+
+app.get('/',function (req,res) {
+  res.send('hello world')
+
+})
+
+app.listen(port,()=> console.log(`Listening at: http://localhost:${port}...`))
