f49fc213cad4ddb2d44c57f1ddb39d412d200e64
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-06-14 15:34:17 -0400

    heroku

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  socketPath: '/var/run/mysqld/mysqld.sock'
 })
 
 connection.connect();
